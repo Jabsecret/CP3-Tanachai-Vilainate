1aa4d059b0e05efbc131804e2bdf545be1369929
Jabsecret <jabtanachai@gmail.com>
2020-11-24 22:35:11 +0700

    Ex.4

---
+++ Assignment/Exercise4_Tanachai_V.py
@@ -0,0 +1,12 @@
+FE   = "Foundation English :"
+GB   = "General Business :"
+ICS  = "Introduction to Computer Systems :"
+CP   = "Computer Programming :"
+x = 30
+y = 20
+z = 15.5
+d = 20.5
+print(FE,x)
+print(GB,y)
+print(ICS,z)
+print(CP,d)
